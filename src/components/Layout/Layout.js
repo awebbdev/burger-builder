90978f606e48738958d7692c840d467c9eb88234
thebigbadox <adamwebb13@gmail.com>
2020-08-09 19:53:25 -0500

    Added Burger and Ingredient Components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Aux from '../../hoc/Auxillary'
+import Aux from '../../hoc/Auxillary';
+import classes from './Layout.module.css';
 
 const layout = ( props ) => (
     <Aux>
         <div>Tooldbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
